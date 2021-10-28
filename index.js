57b01a2510b0a2e09f7a51272e495f5645b1d6cc
gilroymenezes-github <gilroymenezes@hotmail.com>
2021-10-28 11:36:34 +0530

    no reload required

--- index.js
+++ index.js
@@ -10,32 +10,15 @@ const DARK_STYLE_LINK = document.getElementById("dark-theme-style");
 const THEME_TOGGLER = document.getElementById("theme-toggler");
 
 let isDark = LOCAL_META_DATA && LOCAL_META_DATA.isDark;
-let isReload = false;
-
-let navbarElement = document.querySelector('.navbar');
-let navbarBrandElement = document.querySelector('.navbar-brand');
-let footerElement = document.querySelector('.footer'); 
-let footerTextElement = document.querySelector('.footer-text');
-let buttons = document.querySelectorAll('.btn');
 
 const enableDarkTheme = () => {
     DARK_STYLE_LINK.setAttribute("href", DARK_THEME_PATH);
     THEME_TOGGLER.innerHTML = "🌙 Dark";
-    navbarElement.classList.toggle("bg-dark");
-    navbarBrandElement.classList.toggle("text-light");
-    footerElement.classList.toggle("bg-dark");
-    footerTextElement.classList.toggle("text-light");
-    buttons.forEach(b => b.classList.toggle("text-light"));
 }
   
 const disableDarkTheme = () => {
     DARK_STYLE_LINK.setAttribute("href", "");
     THEME_TOGGLER.innerHTML = "🌞 Light";
-    navbarElement.classList.toggle("bg-light");
-    navbarBrandElement.classList.toggle("text-dark");
-    footerElement.classList.toggle("bg-light");
-    footerTextElement.classList.toggle("text-dark");
-    buttons.forEach(b => b.classList.toggle("text-dark"));
 }
 
 
@@ -60,6 +43,5 @@ function toggleTheme() {
   }
   const META = { isDark };
   localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(META));
-  window.location.reload();
 }
 
